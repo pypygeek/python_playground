9a45bd4c3bd60753d3c269b65b606304c9cc0357
서벨롭 <pypygeek@gmail.com>
2022-08-16 03:18:36 +0000

    first

---
+++ python_OOP/class_instance.py
@@ -0,0 +1,25 @@
+class User:
+    pass
+
+# 인스턴스
+user1 = User()
+user2 = User()
+user3 = User()
+
+# 속성 추가
+user1.name = "파이썬"
+user1.email = "python@pthon.python"
+user1.password = "python"
+
+user2.name ="C"
+user2.email = "C@C.C"
+user2.password = "C"
+
+user3.name = "JAVA"
+user3.email = "JAVA"
+user3.password = "JAVA"
+
+# 인스턴스 사용
+print(user1.email)
+print(user2.password)
+print(user3.name)
